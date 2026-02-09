7e805c775dd003678aa014241292a2be1efc6ac5
Ronald "Trey" Menna <30609960+Trey-Menna@users.noreply.github.com>
2026-02-09 02:21:59 -0800

    Update 128. Longest Consecutive Sequence.ts

--- 128. Longest Consecutive Sequence.ts
+++ 128. Longest Consecutive Sequence.ts
@@ -9,6 +9,7 @@ function longestConsecutive(nums: number[]): number {
 //check edge case
     if(size == 0){
       return 0}
+//need to use maxCon
     for ( var i = 0; i < size; i++){
        if (nums[i] == current + 1){
           current = nums[i];
