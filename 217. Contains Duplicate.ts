a07e4399d27331c0d5916bb81cfb66096980b5ea
Trey-Menna <treymenna@sbcglobal.net>
2026-07-02 18:08:06 -0500

    Updating 217 Answer

--- 217. Contains Duplicate.ts
+++ 217. Contains Duplicate.ts
@@ -2,5 +2,15 @@
 //Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
 function containsDuplicate(nums: number[]): boolean {
-    
+	//Declared Variables
+	let numMap: Map<number, string> = new Map();
+	for (let i = 0; i < nums.length; i++) {
+		if (numMap.has(nums[i])) {
+			return true;
+		}
+		else {
+			numMap.set(nums[i], i);
+		}
+	}
+	return false;
 };
