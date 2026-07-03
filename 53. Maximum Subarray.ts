7b4b678dec5b9ecd4e53b6942f0baaae7887e1d4
Trey-Menna <treymenna@sbcglobal.net>
2026-07-03 15:43:38 -0500

    Creating Problem 53

---
+++ 53. Maximum Subarray.ts
@@ -0,0 +1,7 @@
+//Given an integer array nums, find the subarray with the largest sum, and return its sum.
+
+//A subarray is a contiguous non-empty sequence of elements within an array.
+
+function maxSubArray(nums: number[]): number {
+    
+};
