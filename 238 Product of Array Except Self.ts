b124b0c51f7fff485203d32986e82871d07e3ee5
Trey-Menna <treymenna@sbcglobal.net>
2026-07-03 03:08:44 -0500

    Updated Answer for Problem 238 and added a test case

--- 238 Product of Array Except Self.ts
+++ 238 Product of Array Except Self.ts
@@ -4,6 +4,25 @@
 
 //You must write an algorithm that runs in O(n) time and without using the division operation.
 
-	function productExceptSelf(nums: number[]): number[] {
-    
-};
+function productExceptSelf(nums: number[]): number[] {
+	const result = new Array(nums.length).fill(1);
+	//Use two-pass algorithm
+	//Pass 1: Left to right
+	let prefix = 1;
+	for (let i = 0; i < nums.length; i++) {
+		result[i] = prefix;
+		prefix *= nums[i];
+	}
+
+	//Pass 2: Right to Left
+	let suffix = 1;
+	for (let i = nums.length - 1; i >= 0; i--) {
+		result[i] *= suffix;
+		suffix *= nums[i];
+	}
+	return result;
+}
+
+// Test case
+console.log(productExceptSelf([1, 2, 3, 4]));
+// Expected Output: [24, 12, 8, 6]
