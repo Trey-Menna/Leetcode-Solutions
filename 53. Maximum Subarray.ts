a6ca3ffc3a96e5e003229d6c8e3691f986c13758
Trey-Menna <treymenna@sbcglobal.net>
2026-07-04 14:50:10 -0500

    Upading Porblem 53's Answer

--- 53. Maximum Subarray.ts
+++ 53. Maximum Subarray.ts
@@ -3,19 +3,18 @@
 //A subarray is a contiguous non-empty sequence of elements within an array.
 
 function maxSubArray(nums: number[]): number {
-	let maxSub = 0;
+	let maxSub = -Infinity; //Why did Infinity help with arrays with only negative numbers
 	let currentSub = 0;
-	for(let i =0; i < nums.length; i++){
-		currentSub = currentSub +  nums[i];
-		if(currentSub > maxSub){
-			maxSub = currentSub
-			currentSub = nums[i];
+	for (let i = 0; i < nums.length; i++) {
+		currentSub = Math.max((currentSub + nums[i]), nums[i]);
+		if (currentSub > maxSub) {
+			maxSub = currentSub;
 		}
-		else if(currentSub < 0){
+		else if (currentSub < 0) {
 			currentSub = 0;
 		}
 	}
 	return maxSub;
 }
 // Test with expected output of 6
-console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]));
+console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
