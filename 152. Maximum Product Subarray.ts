0e687a68ef39f56186101dc19427217ece38463d
Trey-Menna <treymenna@sbcglobal.net>
2026-07-04 14:53:06 -0500

    Creating Problem 152

---
+++ 152. Maximum Product Subarray.ts
@@ -0,0 +1,12 @@
+/*Given an integer array nums, find a subarray that has the largest product, and return the product.
+
+The test cases are generated so that the answer will fit in a 32-bit integer.
+
+Note that the product of an array with a single element is the value of that element. */
+function maxProduct(nums: number[]): number {
+    
+};
+
+Console.log(maxProduct([2,3,-3,4]));
+//Should Log 6 since 2 * 3 is 6
+
