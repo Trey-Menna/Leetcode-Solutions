8fe2f77224f1dafd4453ea6ec4d569f143acbf4b
Trey-Menna <treymenna@sbcglobal.net>
2026-07-05 17:00:17 -0500

    Updating Answer for Problem 152

--- 152. Maximum Product Subarray.ts
+++ 152. Maximum Product Subarray.ts
@@ -4,20 +4,29 @@ The test cases are generated so that the answer will fit in a 32-bit integer.
 
 Note that the product of an array with a single element is the value of that element. */
 function maxProduct(nums: number[]): number {
- 	let maxSub = -Infinity;
-	let currentSub = 0;
-	for(let i =0; i < nums.length; i++){
-		currentSub = Math.max((currentSub * nums[i]), nums[i]);
-		if(currentSub > maxSub){
-			maxSub = currentSub;
-		}
-		else if(currentSub < 0){
-			currentSub = 0;
-		}
+	if (nums.length === 0) return 0;
+
+	let maxSub = nums[0];
+	let minSub = nums[0];
+	let globalMax = nums[0];
+
+	for (let i = 1; i < nums.length; i++) {
+		const num = nums[i];
+
+		// Store current max before it gets overwritten
+		const tempMax = maxSub;
+
+		// Calculate all three possibilities for the new boundaries
+		maxSub = Math.max(num, tempMax * num, minSub * num);
+		minSub = Math.min(num, tempMax * num, minSub * num);
+
+		// Update the highest product seen so far
+		globalMax = Math.max(globalMax, maxSub);
 	}
-	return maxSub;
+
+	return globalMax;
 };
 
-console.log(maxProduct([2,3,-3,4]));
+console.log(maxProduct([2, 3, -3, 4]));
 //Should Log 6 since 2 * 3 is 6
 
