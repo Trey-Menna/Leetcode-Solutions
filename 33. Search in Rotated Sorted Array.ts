507831592004ff1cb3688ca2ed3cfb1316ec50f8
Trey-Menna <treymenna@sbcglobal.net>
2026-07-06 15:22:45 -0500

    Updating Answer 33 with a modified Binary Search

--- 33. Search in Rotated Sorted Array.ts
+++ 33. Search in Rotated Sorted Array.ts
@@ -7,8 +7,35 @@ Given the array nums after the possible rotation and an integer target, return t
 You must write an algorithm with O(log n) runtime complexity.*/
 
 function search(nums: number[], target: number): number {
-    
+	let left = 0
+	let right = nums.length - 1;
+
+	while (left <= right) {
+		let mid = Math.floor((left + right) / 2);
+		if (nums[mid] == target) {
+			return mid;
+		}
+		// Check if Left half is sorted
+		if (nums[left] <= nums[mid]) {
+			if (nums[left] <= target && target < nums[mid]) {
+				right = mid - 1;
+			}
+			else {
+				left = mid + 1;
+			}
+		} else {
+			//Otherwise, Right hald has to be sorted
+			if (nums[mid] < target && target <= nums[right]) {
+				left = mid + 1;
+			}
+			else {
+				right = mid - 1;
+			}
+		}
+	}
+	return -1;
+
 };
 
-console.log(search([4,5,6,7,0,1,2],0));
+console.log(search([4, 5, 6, 7, 0, 1, 2], 0));
 //Expected Output is 4
