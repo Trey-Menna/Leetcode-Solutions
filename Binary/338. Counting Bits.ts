c993772826fe6244a8b763d21876a1774cf7e9e2
Trey-Menna <treymenna@sbcglobal.net>
2026-07-18 19:34:55 -0500

    Created Problem 338

---
+++ Binary/338. Counting Bits.ts
@@ -0,0 +1,8 @@
+//Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+
+function countBits(n: number): number[] {
+    
+};
+
+console.log(countBits(2));
+//Output should be [0,1,1]
