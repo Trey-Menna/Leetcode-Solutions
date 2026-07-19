7c2b5eae600cf3fd4504223c03736b7d700f0f99
Trey-Menna <treymenna@sbcglobal.net>
2026-07-18 19:42:14 -0500

    Solved Problem 338

--- Binary/338. Counting Bits.ts
+++ Binary/338. Counting Bits.ts
@@ -1,7 +1,16 @@
 //Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
 
 function countBits(n: number): number[] {
-    
+	//Initilize an array with size of n + 1 filled with 0s
+	const ans: number[] = new Array(n + 1).fill(0);
+
+	//Compute each bit count for each number based on previous results
+	for (let i = 1; i <= n; i++) {
+		//ans[1 >> i] shifts bit right (drops last bit)
+		// (i & 1) extracts the last bit (adds 1 if odd, 0 if even)\
+		ans[i] = ans[i >> 1] + (i & 1);
+	}
+	return ans;
 };
 
 console.log(countBits(2));
