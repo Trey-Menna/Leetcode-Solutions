5b2c3fd7d380c16ee71cdaec998e08e93bb00075
Trey-Menna <treymenna@sbcglobal.net>
2026-07-20 08:07:54 -0500

    Created Problem 190

---
+++ Binary/190. Reverse Bits.ts
@@ -0,0 +1,10 @@
+//Reverse bits of a given 32 bits signed integer.
+function reverseBits(n: number): number {
+    
+};
+
+console.log(reverseBits(43261596));
+/* Output: 964176192
+Integer	Binary
+43261596	00000010100101000001111010011100
+964176192	00111001011110000010100101000000 */
