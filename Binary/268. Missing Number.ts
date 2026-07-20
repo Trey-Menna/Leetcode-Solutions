76f112ccc2d92d16bd4a6395a9a1c1569840608d
Trey-Menna <treymenna@sbcglobal.net>
2026-07-19 23:18:06 -0500

    Solved Problem 268

--- Binary/268. Missing Number.ts
+++ Binary/268. Missing Number.ts
@@ -1,6 +1,11 @@
 //Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
 function missingNumber(nums: number[]): number {
-    
+   let missing = nums.length;
+
+   for(let i = 0; i < nums.length; i++){
+	missing ^= i ^ nums[i];
+   }
+   return missing;
 };
 
 console.log(missingNumber([3,0,1]));
