9313ec388a6ffe70583ac62f93b7a99b3ff2e763
Trey-Menna <treymenna@sbcglobal.net>
2026-07-20 08:31:41 -0500

    Solved Problem 190

--- Binary/190. Reverse Bits.ts
+++ Binary/190. Reverse Bits.ts
@@ -1,6 +1,12 @@
 //Reverse bits of a given 32 bits signed integer.
 function reverseBits(n: number): number {
-    
+	let result = 0;
+	for (let i = 0; i < 32; i++) {
+		result = (result << 1) | (n & 1);
+		n >>= 1;
+	}
+	return result >>> 0;
+
 };
 
 console.log(reverseBits(43261596));
