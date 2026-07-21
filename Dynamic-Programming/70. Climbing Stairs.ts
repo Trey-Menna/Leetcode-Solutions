7a237c7ae8fc1943c513e3530bcc0ef5624803a2
Trey-Menna <treymenna@sbcglobal.net>
2026-07-21 15:59:49 -0500

    Created Dynamic Programming Folder and Question 70

---
+++ Dynamic-Programming/70. Climbing Stairs.ts
@@ -0,0 +1,13 @@
+//You are climbing a staircase. It takes n steps to reach the top.
+
+//Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+function climbStairs(n: number): number {
+    
+};
+
+console.log(climbStairs(2))
+//output: 2 
+/*Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps*/
