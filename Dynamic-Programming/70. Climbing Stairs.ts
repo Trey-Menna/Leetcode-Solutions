004d10a14c843c8e068930fbfb9b9b524124b6cf
Trey-Menna <treymenna@sbcglobal.net>
2026-07-21 16:11:03 -0500

    Completed Problem 70

--- Dynamic-Programming/70. Climbing Stairs.ts
+++ Dynamic-Programming/70. Climbing Stairs.ts
@@ -3,7 +3,24 @@
 //Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 
 function climbStairs(n: number): number {
-    
+	//Edge case if n is 1 or 2, result equals n 
+	if (n <= 2) {
+		return n;
+	}
+
+	//Initilize Tracking Variables
+	let prev2: number = 1;
+	let prev1: number = 2;
+	let current: number = 0;
+
+	//Iteratively compute ways for subsequent steps up to n 
+	for (let i = 3; i <= n; i++) {
+		current = prev1 + prev2;
+		prev2 = prev1;
+		prev1 = current;
+	}
+
+	return current;
 };
 
 console.log(climbStairs(2))
