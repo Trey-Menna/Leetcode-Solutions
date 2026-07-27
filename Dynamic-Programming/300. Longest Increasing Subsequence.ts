f8d31bae99b1529de811e6f5fb64dca09cfa6c1b
Trey-Menna <treymenna@sbcglobal.net>
2026-07-27 16:38:00 -0500

    Solved Problem 300

--- Dynamic-Programming/300. Longest Increasing Subsequence.ts
+++ Dynamic-Programming/300. Longest Increasing Subsequence.ts
@@ -1,7 +1,21 @@
 //Given an integer array nums, return the length of the longest strictly increasing subsequence.
 
 function lengthOfLIS(nums: number[]): number {
-    
+   if(nums.length === 0) return 0;
+
+   //Initilize DP array with 1, as a single element is a subsequence of lenght 1
+   const dp: number[] = new Array(nums.length).fill(1);
+   let maxLength = 1;
+
+   for(let i = 1; i < nums.length; i++){
+	for(let j = 0; j < i; j++){
+		if(nums[i] > nums[j]){
+			dp[i] = Math.max(dp[i], dp[j] + 1);
+		}
+	}
+	maxLength = Math.max(maxLength, dp[i]);
+   }
+   return maxLength;
 };
 
 console.log(lengthOfLIS([10,9,2,5,3,7,101,18]));
