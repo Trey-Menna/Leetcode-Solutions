9d19b1120da4b0c3134e1068e9323b88be79cb2e
Trey-Menna <treymenna@sbcglobal.net>
2026-07-27 16:30:33 -0500

    Created Problem 300

---
+++ Dynamic-Programming/300. Longest Increasing Subsequence.ts
@@ -0,0 +1,9 @@
+//Given an integer array nums, return the length of the longest strictly increasing subsequence.
+
+function lengthOfLIS(nums: number[]): number {
+    
+};
+
+console.log(lengthOfLIS([10,9,2,5,3,7,101,18]));
+/*Output: 4
+Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.*/
