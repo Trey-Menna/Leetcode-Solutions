e103a56049f45f1ac52c4c7c7fc4b148e535e9f2
Trey-Menna <treymenna@sbcglobal.net>
2026-07-28 13:17:52 -0500

    Solved Problem 1143

--- Dynamic-Programming/1143. Longest Common Subsequence.ts
+++ Dynamic-Programming/1143. Longest Common Subsequence.ts
@@ -6,9 +6,29 @@ For example, "ace" is a subsequence of "abcde".
 A common subsequence of two strings is a subsequence that is common to both strings.*/
 
 function longestCommonSubsequence(text1: string, text2: string): number {
-    
+   const m = text1.length;
+   const n = text2.length;
+
+   //Create an (m + 1) x (n + 1) DP table initialized with 0
+   const dp: number[][] = Array.from({length: m + 1}, ()=> new Array(n + 1).fill(0));
+
+   /*Iterate through both strings using a nested loop. If the current characters match, add 1 to the diagonal top-left value. 
+   If they do not match, take the maximum value between the cell above and the cell to the left.*/
+   for(let i = 1; i <= m; i++){
+	for(let j = 1; j <= n; j++){
+		if(text1[i - 1] === text2[j -1]){
+			//Characters match: take diagonal value + 1
+			dp[i][j] = dp[i -1][j-1] + 1
+		}
+		else{
+			//Characters mismatch: take max of top or left neighbor
+			dp[i][j] = Math.max(dp[i -1][j], dp[i][j - 1]);
+		}
+	}
+   }
+   return dp[m][n]
 };
 
-console.log(longestCommonSubsequence(abcde, ace));
+console.log(longestCommonSubsequence("abcde", "ace"));
 /*Output: 3  
 Explanation: The longest common subsequence is "ace" and its length is 3.*/
