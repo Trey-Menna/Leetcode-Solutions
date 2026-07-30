affdd4bf87c2f40f01040e35a0eb34a357ad978e
Trey-Menna <treymenna@sbcglobal.net>
2026-07-30 14:58:50 -0500

    Solved Problem 377

--- Dynamic-Programming/377. Combination Sum IV.ts
+++ Dynamic-Programming/377. Combination Sum IV.ts
@@ -3,7 +3,21 @@
 The test cases are generated so that the answer can fit in a 32-bit integer.*/
 
 function combinationSum4(nums: number[], target: number): number {
-    
+   // dp[i] will store the number of combinations that sum up to i 
+   const dp: number[] = new Array(target + 1).fill(0);
+
+   //Base case: There is 1 way to make the sum 0 (empty array)
+   dp[0] = 1;
+
+   // Fill the dp array iteratively from 1 up to target
+   for(let i = 1; i <= target; i++){
+	for(const num of nums){
+		if(i >= num){
+			dp[i] += dp[i- num];
+		}
+	}
+   }
+   return dp[target];
 };
 
 console.log(combinationSum4([1,2,3],4));
