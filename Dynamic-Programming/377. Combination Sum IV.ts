0a694b0cb51ff186ef019118614c1df54613387f
Trey-Menna <treymenna@sbcglobal.net>
2026-07-30 14:56:08 -0500

    Created Problem 377

---
+++ Dynamic-Programming/377. Combination Sum IV.ts
@@ -0,0 +1,20 @@
+/*Given an array of distinct integers nums and a target integer target, return the number of possible combinations that add up to target.
+
+The test cases are generated so that the answer can fit in a 32-bit integer.*/
+
+function combinationSum4(nums: number[], target: number): number {
+    
+};
+
+console.log(combinationSum4([1,2,3],4));
+/*Output: 7
+Explanation:
+The possible combination ways are:
+(1, 1, 1, 1)
+(1, 1, 2)
+(1, 2, 1)
+(1, 3)
+(2, 1, 1)
+(2, 2)
+(3, 1)
+Note that different sequences are counted as different combinations.*/
