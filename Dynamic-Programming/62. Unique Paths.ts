f2ff01c2ccfa0a7457279a3d806ab8af71db3a66
Trey-Menna <treymenna@sbcglobal.net>
2026-08-06 14:29:43 -0500

    Solved Problem 62

--- Dynamic-Programming/62. Unique Paths.ts
+++ Dynamic-Programming/62. Unique Paths.ts
@@ -5,7 +5,18 @@ Given the two integers m and n, return the number of possible unique paths that 
 The test cases are generated so that the answer will be less than or equal to 2 * 109.*/
 
 function uniquePaths(m: number, n: number): number {
-    
+   // Initilize an array of size n with 1s representing the top row
+   const row: number[] = new Array(n).fill(1);
+
+   // Iterate through each row starting from second row (index 1)
+   for(let i = 1; i < m; i++){
+	//Update each column value from left to right 
+	for(let j = 1; j < n; j++){
+		row[j] += row[j-1];
+	}
+   }
+   // The last element represents the bottom-right corner 
+   return row[n - 1];
 };
 
 console.log(uniquePaths(3,7));
